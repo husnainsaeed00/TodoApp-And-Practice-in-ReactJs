68ca699d1b847dabaa4366fb2b29692996e25361
Husnain Saeed <husnainsaeed000@gmail.com>
2023-08-04 04:02:32 -0700

    class based components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-import React from "react";
-import  ReactDOM from "react-dom";
-import App from "./App";
-import './index.css'; 
-// Parent Child Components in the React
-// Always keep practing keep builing stuff to stay on the skill
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Husnain saeed" />
+        <Greating />
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+class Header extends React.Component {
+  render() {
+    return (
+      <p>Welcome to the Class components {this.props.username}</p>
+    );
+  }
+}
+
+class Greating extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+    return <h1>Good {timeOfDay} to you Sir!</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

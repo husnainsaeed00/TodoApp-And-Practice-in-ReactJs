9bee3a4974b839e68f546e5219f175b680b55d13
Husnain Saeed <husnainsaeed000@gmail.com>
2023-08-05 02:43:56 -0700

    conditional rendering in class components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-// import TodoItem from './Components/TodoItem';
-// import TodosData from './Components/TodosData';
-// higher order functions are used and in that func
-// we are using the builtIn map funcition to loop thrrough
-// the json file and create a new array .
-// that higher order funciton is returning 
-// functional component <Product/>
-// function App() {
-//   const todoitem=TodosData.map(item => <TodoItem key={item.id} item={item}/> )
-//   return(
-//     <div className='TodoList'>
-//       {todoitem}
-//     </div>
-//   )
-// }
-
-
-// after learning about functional components 
-// we are going to learn about class Components of react
+import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn:true,
+    };
+  }
+
   render() {
-    const date=new Date()
+    const { isLoggedIn } = this.state; // Destructuring the state
+
     return (
       <div>
-        <h1>Software Engineering</h1>
+        {isLoggedIn ? (
+          <h1>You are currently logged in</h1>
+        ) : (
+          <h1>You are currently logged out</h1>
+        )}
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/index.js
+++ src/index.js
@@ -1,39 +1,47 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+ import React, { Component } from "react";
+ import ReactDOM from "react-dom";
+ import App from "./App";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header username="Husnain saeed" />
-        <Greating />
-      </div>
-    );
-  }
-}
+// class App extends React.Component {
+//     render(){
+//     return (
+//       <div>
+//         <Header username="Husnain saeed khan" /> 
+//         <Greating />
+//       </div> 
+//     );
+//   }
+// }
+// class Header extends React.Component{
+//     render(){
+//     return( <p>Welcome to the Class components {this.props.username}</p>)
 
-class Header extends React.Component {
-  render() {
-    return (
-      <p>Welcome to the Class components {this.props.username}</p>
-    );
-  }
-}
-
-class Greating extends Component {
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "night";
-    }
-    return <h1>Good {timeOfDay} to you Sir!</h1>;
-  }
-}
+// }
+// }
+// class Greating extends React.Component {
+//     render(){
+//     const date = new Date();
+//     const hours = date.getHours();
+//     let timeOfDay;
+//     if (hours < 12) {
+//       timeOfDay = "morning";
+//     } else if (hours >= 12 && hours < 17) {
+//       timeOfDay = "afternoon";
+//     } else {
+//       timeOfDay = "night";
+//     }
+//     return <h1>Good {timeOfDay} to you Sir!</h1>;
+//   }
+// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// return method is compulsory for the  Functional 
+// Components and render method is the compulsory for the
+// class components.
+//return mehtod in functional components returns Xml
+//render method also returs xml
+//Funcional components are statelss untill we use
+//React hooks
+//class components are used for statefull and more complex
+// web applications.

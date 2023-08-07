1c95b4d2770152c9aa74afbf95e2e33a087ebfa5
Husnain Saeed <husnainsaeed000@gmail.com>
2023-08-07 05:40:12 -0700

    Full Form Practice Done

--- src/App.js
+++ src/App.js
@@ -250,7 +250,98 @@
 
 // export default App;
 
+// import React from "react";
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       firstName: "",
+//       lastName: "",
+//       isFriendly:false,
+//       gender:'',
+//       favColor:'blue'
+//     };
+//     this.handleChange=this.handleChange.bind(this)
+//   }
+//  handleChange(event){
+//   // we have used synthetic events it looks like unpacking in python
+//   const {name,value,type,checked} =event.target
+//   type ==='checkbox' ? this.setState({[name]:checked}):this.setState({[name]:value})
+//  }
+//   render() {
+//     return (
+//       <div>
+//         <input
+//         // we are using controlled state in the forms
+//           type="text"
+//           placeholder="fistName"
+//           name="firstName"
+//           value={this.state.firstName}
+//           onChange={this.handleChange}
+//         />
+//         <br/>
+//         <input
+//           type="text"
+//           placeholder="lastName"
+//           name="lastName"
+//           value={this.state.lastName}
+//           onChange={this.handleChange}
+//         />
+//         <br/>
+//         <textarea
+//           value={"some default values"}
+//           onChange={this.handleChange}
+//         />
+//         <br/>
+//         <label>
+//           <input type="checkbox"
+//             name="isFriendly"
+//             checked={this.state.isFriendly}
+//             onChange={this.handleChange}
+//           />Is Friendly
+//         </label>
+//         <br/>
+//         <label>
+//           <input type="radio"
+//             name="gender"
+//             value='male'
+//             checked={this.state.gender ==='male'}
+//             onChange={this.handleChange}
+//           />Male
+//         </label>
+//         <br/>
+//         <label>
+//           <input type="radio"
+//             name="gender"
+//             value='female'
+//             checked={this.state.gender ==='female'}
+//             onChange={this.handleChange}
+//           />FeMale
+//         </label>
+//         <br/>
+//         <select
+//           value={this.state.favColor}
+//           onChange={this.handleChange}
+//           name="favColor">
+//           <option value='blue'> blue</option>
+//           <option value='green'> green</option>
+//           <option value='red'> red</option>
+//           <option value='black'>black</option>
+//         </select>
+//         <h1>{this.state.firstName} {this.state.lastName}</h1>
+//         <h1>You are a {this.state.gender}</h1>
+//       </div>
+//     );
+//   }
+// }
+// //-----Formik library takes your pain of forms creation in React
+// export default App;
+
+// ========================Evening of 07-08-2023==========================
+
 import React from "react";
+import './index.css';
 
 class App extends React.Component {
   constructor() {
@@ -258,48 +349,59 @@ class App extends React.Component {
     this.state = {
       firstName: "",
       lastName: "",
-      isFriendly:false,
-      gender:'',
-      favColor:'blue'
+      age:'',
+      gender: "",
+      destination: "Uk",
+      isVegan:false,
+      isKoshar:false,
+      isLactosFree:false
+    
     };
-    this.handleChange=this.handleChange.bind(this)
+    this.handleChange=this.handleChange.bind(this);
   }
- handleChange(event){
-  // we have used synthetic events it looks like unpacking in python
-  const {name,value,type,checked} =event.target
-  type ==='checkbox' ? this.setState({[name]:checked}):this.setState({[name]:value})
- }
+  handleChange(event){
+    const {name,value,type,checked} = event.target
+    type ==='checkbox' ?
+      this.setState({
+            [name]:checked
+          })
+    :
+    this.setState({
+      [name]:value
+    })
+  }
   render() {
     return (
-      <div>
-        <input
-        // we are using controlled state in the forms  
-          type="text"
-          placeholder="fistName"
-          name="firstName"
-          value={this.state.firstName}
-          onChange={this.handleChange}
-        />
-        <br/>
-        <input
-          type="text"
-          placeholder="lastName"
-          name="lastName"
-          value={this.state.lastName}
-          onChange={this.handleChange}
-        />
-        <br/>
-        <textarea
-          value={"some default values"}
-          onChange={this.handleChange}
-        />
-        <br/>
-        <label>
-          <input type="checkbox"
-            name="isFriendly"
-            checked={this.state.isFriendly}
+      <main>
+        <form className="TodoList">
+          <input
+            name="firstName"
+            value={this.state.firstName}
             onChange={this.handleChange}
-          />Is Friendly
+            placeholder="firstName"
+          />
+          <br/>
+          <input
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+            placeholder="lastName"
+          />
+          <br/>
+          <input
+            name="age"
+            value={this.state.age}
+            onChange={this.handleChange}
+            placeholder="Age"
+          />
+          <br/>
+          <label>
+          <input type="radio"
+            name="gender"
+            value='female'
+            checked={this.state.gender ==='female'}
+            onChange={this.handleChange}
+          />FeMale
         </label>
         <br/>
         <label>
@@ -312,28 +414,60 @@ class App extends React.Component {
         </label>
         <br/>
         <label>
-          <input type="radio"
-            name="gender"
-            value='female'
-            checked={this.state.gender ==='female'}
-            onChange={this.handleChange}
-          />FeMale
+        <select
+          value={this.state.destination}
+          onChange={this.handleChange}
+          name="destination">
+          <option value='USA'> USA</option>
+          <option value='UK'> Uk</option>
+          <option value='Estonia'> Estonia</option>
+          <option value='Japan'>Japan</option>
+        </select>Your Destination
         </label>
         <br/>
-        <select
-          value={this.state.favColor}
-          onChange={this.handleChange}
-          name="favColor">
-          <option value='blue'> blue</option>
-          <option value='green'> green</option>
-          <option value='red'> red</option>
-          <option value='black'>black</option>
-        </select>
-        <h1>{this.state.firstName} {this.state.lastName}</h1>
-        <h1>You are a {this.state.gender}</h1>
-      </div>
+        <label>
+          <input 
+            type="checkbox"
+            name="isVegan"
+            onChange={this.handleChange}
+            checked={this.state.isVegan}
+          />vegan
+        </label>
+        <br/>
+        <label>
+          <input 
+            type="checkbox"
+            name="isLactosFree"
+            onChange={this.handleChange}
+            checked={this.state.isLactosFree}
+          />Is LactosFree
+        </label>
+        <br/>
+        <label>
+          <input 
+            type="checkbox"
+            name="isKoshar"
+            onChange={this.handleChange}
+            checked={this.state.isKoshar}
+          />Koshar
+        </label>
+        <br/>
+        <button>Submit</button>
+        </form>
+        <hr/>
+        <h2>Entered Information:</h2>
+        <p>Your Name: {this.state.firstName} {this.state.lastName}</p>
+        <p>Your Age: {this.state.age}</p>
+        <p>Your gender:{this.state.gender}</p>
+        <p>Your destination:{this.state.destination}</p>
+        <p>Your diet:
+          Vegan:{this.state.isVegan ? "Yes": "No"}</p>
+          <p>Your diet:
+          Koshar:{this.state.isKoshar ? "Yes": "No"}</p>
+          <p>Your diet:
+          LactosFree:{this.state.isLactosFree ? "Yes": "No"}</p>
+      </main>
     );
   }
 }
-//-----Formik library takes your pain of forms creation in React
 export default App;
